2e41fe1617eeb1cd2052957cd5ec7538a7d797bd
wandoy37 <riswandi035@gmail.com>
2025-09-08 22:34:37 +0800

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/logo.png') }}">
+
     <title>{{ $title ?? 'Page Title' }}</title>
 
     <!-- Bootstrap CSS -->
